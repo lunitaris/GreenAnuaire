cf9e818aea2bc727014384a3e512666429dd59bb
Lunitaris <mael.lepicard@gmail.com>
2015-07-21 15:44:06 +0200

    Update GreenServer.py

--- GreenServer.py
+++ GreenServer.py
@@ -7,35 +7,44 @@ if(len(sys.argv) != 2):
     print("Ex:    # python3 GreenServer.py 666")
     sys.exit()
 
-
-import pickle 
-import socket, threading, time
-import uuid
-import hashlib
+import socket, threading, pickle
+import uuid, hashlib
+import signal, time
 
 HOST = '127.0.0.1'
 PORT = int(sys.argv[1])
-
-# Variable annuaire globale
-global GreenAnuaire
-GreenAnuaire = {}
+global mySocket     ## variable globale qui servira a la fonction lance lors d'un SIGINT pour fermer le socket et quitter proprementself.
 
 
-
-# Fonctions de verification de mot de passe.==================================================;
-
-### Fonction qui retourne le mot de passe hashe
+### Fonction qui retourne le hash du mot entré en paramètre.
 def hash_password(password):
     # uuid is used to generate a random number
     salt = uuid.uuid4().hex
     return hashlib.sha256(salt.encode() + password.encode()).hexdigest() + ':' + salt
+
+
+
+### Création du dictionnaire avec UN utilisateur par defaut (admin) pour pouvoir administrer l'annuaire
+### Le mot de passe pourra ensuite être modifié, et enregistré dans le fichier annuaire.
+global GreenAnuaire     ## Variable annuaire globale
+ficAnnuaire = "Annuaire"    ## Nom du fichier annuaire à utiliser pour enregistrer / charger l'annuaire
+adminDefaultPassword="green"    ## MOT DE PASSE PAR DEFAUT pour l'utilisateur admin
+
+
+### Compte administrateur par défaut (Login : admin || Password : "green")
+GreenAnuaire = {'admin': ['admin', 'local', 'IloveGreen@admin.com', 'Administrateur par defaut', '-', 'A', hash_password(adminDefaultPassword)]}
+
+### Fonction qui sauvegarde l'annuaire courant dans le fichier pointé par la variable ficAnnuaire
+def saveDico():
+    with open(ficAnnuaire, 'wb') as f:
+        pickle.dump(GreenAnuaire, f)
+
 
 ### Fonction qui verifie si le hash de la string 'user_password' correspond bien au hash passe en parametre (hashed_password) retourne TRUE si vrai, FALSE sinon.
 def check_password(hashed_password, user_password):
     password, salt = hashed_password.split(':')
     return password == hashlib.sha256(salt.encode() + user_password.encode()).hexdigest()
 
-#============================================================================================;
 
 ### Retourne l'annuaire en string
 def afficherA():
@@ -265,6 +274,7 @@ class ThreadClient(threading.Thread):
             message = "%s> %s" % (nom, msgClient)
             print(message)
 
+
 ###########################################################################################
         # Fermeture de la connexion :
         nomTh=self.getName()
@@ -276,23 +286,28 @@ class ThreadClient(threading.Thread):
 
 #######################################################################################
 
+
+# Decommenter pour insérer quelques utilisteurs dans l'annuaire
 #add("Burtin", "Graig", "burtin.graig@gmail.com", "Fondumonde", "Ingenieur", "083684876", "U")
-# add("lepicard", "mael", "mael.lepicard@gmail.com", "Montrouge", "Ingenieur", "0669123442", "A","azerty")
+#add("lepicard", "mael", "mael.lepicard@gmail.com", "Montrouge", "Ingenieur", "0669123442", "A","azerty")
 #add("Admin1", "Admin1", "admin@hotmail.com", "ESGI", "Ingenieur", "123456789", "A")
 
 
-### Fonction qui sauvegarde l'annuaire courant dans le fichier 'GreenAnuaire.pickle'
-def saveDico():
-    with open('GreenAnuaire.pickle', 'wb') as f:
-        pickle.dump(GreenAnuaire, f)
 
 
-### charge le fichier annuaire déja existant
-with open('GreenAnuaire.pickle', 'rb') as f:
-    GreenAnuaire = pickle.load(f)
-    print("Chargement de l'annuaire : Done \n")
+
+try:     ### charge le fichier annuaire déja existant. Si il n'existe pas, il sera créé
+    with open(ficAnnuaire, 'rb') as f:
+        GreenAnuaire = pickle.load(f)       ### Dump le contenu de l'annuaire dans le fichier 
+        print("Chargement du fichier annuaire '",ficAnnuaire,"'")
+except:
+    print("Fichier annuaire non trouvé, création du fichier '",ficAnnuaire,"'")
+    print("Pour se connecter à l'annuaire, connectez vous sur le compte Admin par défaut")
+    print("==> Login: admin, Password: ",adminDefaultPassword,"\n")
+    saveDico()
 
 
+######################  Lancement socket  #################################################################
 # Initialisation du serveur - Définition du socket :
 mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
@@ -302,20 +317,27 @@ except socket.error:
     print ("La liaison du socket à l'adresse choisie a échoué.")
     sys.exit()
 
-# On continue en séquence
 print( "Serveur prêt, en attente de requêtes ...")
-mySocket.listen(5)
+print( "Appuyez sur CTRL+C  pour quitter\n")
 
-# Attente et prise en charge des connexions demandées par les clients :
+### Intercepte le signal SIGINT (ctrl+c) et termine proprement le programme.
+def sigint_handler(signum, frame):
+    global mySocket
+    print("\nExiting GreenAnnuaire...")
+    mySocket.close()
+    sys.exit()
+ 
+signal.signal(signal.SIGINT, sigint_handler)    # handle le sigint, et lance la fonction sigint_handler
+
+mySocket.listen(5)  # Attente et prise en charge des connexions demandées par les clients :
 conn_client = {}                # dictionnaire des connexions clients
 
 while 1:    
     connexion, adresse = mySocket.accept()
-
-    # Créer un nouvel objet thread pour gérer la connexion :
+    ### Créer un nouvel objet thread pour gérer la connexion :
     th = ThreadClient(connexion)
     th.start()
 
-    # Mémoriser la connexion dans le dictionnaire : 
+    ### Mémoriser la connexion dans le dictionnaire : 
     it = th.getName()        # identifiant du thread
     conn_client[it] = connexion
