50c4be217344b3f00b0d98b33ef9d7d7f3d8e3d8
Lunitaris <mael.lepicard@gmail.com>
2015-07-21 14:14:21 +0200

    Update GreenClient.py

--- GreenClient.py
+++ GreenClient.py
@@ -1,89 +1,111 @@
-# Définition d'un client réseau gérant en parallèle l'émission
-# et la réception des messages (utilisation de 2 THREADS).
+import socket,time
 import sys
+from tkinter import *
+from tkinter.messagebox import * # boîte de dialogue
 
-if(len(sys.argv) != 3):
-    print("Usage: # python3 GreenClient.py login port")
-    print("Ex:    # python3 GreenClient.py administrator 666")
+def Connet2serv():
+
+    HOST = addrServ.get()       ### récupération de l'adresse du serveur entré dans l'input box
+    PORT = int(portServ.get())  ### récupération du port du serveur entré dans l'input box
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server_address = (HOST   ,PORT)
+    sock.settimeout(1)
+    print('connecting to %s port %s' % server_address)
+
+    LOGIN = Tlogin.get()
+    PASS = mdp.get()
+
+    try:
+        sock.connect(server_address)
+        sock.send(LOGIN.encode())
+        time.sleep(1)
+        sock.send(PASS.encode())
+
+        data=''
+        time.sleep(2)
+        data = sock.recv(1024)
+
+        print(data.decode())
+        if(data.decode() != "Error"):
+            while True:
+
+                rep = input("["+LOGIN+"]# ")
+                if(len(rep)):
+                    sock.send(rep.encode())
+                if(rep == "FIN"):
+                    print()
+                    break
+
+                try:
+                    data = sock.recv(1024)
+                    print('%s' % data.decode())
+                except socket.timeout:              ### Si rien n'est envoyé par le serveur au bout de 1 seconde
+                   pass
+
+    except socket.error:
+        print ("La connexion a échoué...")
+        showwarning('Résultat','Erreur de connection.\n!')
+        
+        #sock.shutdown(socket.SHUT_RDWR)
+        sock.close()
+        sys.exit()
+
+    sock.close()
     sys.exit()
 
-import socket, threading, time
+
+Mafenetre = Tk()
+Mafenetre.title('Connexion Annuaire')
 
 
-HOST = '127.0.0.1'
-PORT = int(sys.argv[2])
+####################################    Login   #######################################
+### Label pour afficher 'login'
+Label1 = Label(Mafenetre, text = 'Login')
+Label1.pack(side = LEFT, padx = 5, pady = 5)
 
-# Variable globale d'arret de thread. Si celle-ci est à 1, tous les threads se coupent
-global iKillYouBloodyThread
-iKillYouBloodyThread = 0
+# Création d'un widget Entry (champ de saisie) pour le login
+Tlogin= StringVar()
+Champ = Entry(Mafenetre, textvariable= Tlogin, bg ='bisque', fg='maroon')
+Champ.focus_set()
+Champ.pack(side = LEFT, padx = 5, pady = 5)
 
 
+####################################    MDP   #######################################
+### Label pour afficher 'login'
+Label2 = Label(Mafenetre, text = 'Password')
+Label2.pack(side = LEFT, padx = 5, pady = 5)
 
-#-----------------------------------------------------------------------
-class ThreadReception(threading.Thread):
-    """objet thread gérant la réception des messages"""
-    def __init__(self, conn):
-        threading.Thread.__init__(self)
-        self.connexion = conn           # réf. du socket de connexion
-        
-    def run(self):
-        global iKillYouBloodyThread     # variable d'arret du thread.
-        while iKillYouBloodyThread == 0:
-            msgServerEncoded = self.connexion.recv(1024)
-            message_recu = msgServerEncoded.decode()
+# Création d'un widget Entry (champ de saisie) pour le login
+mdp= StringVar()
+Champ = Entry(Mafenetre, textvariable= mdp, bg ='bisque', fg='maroon')
+Champ.focus_set()
+Champ.pack(side = LEFT, padx = 5, pady = 5)
 
-            message = "%s> %s" % ("Server: ", message_recu)
-            print(message)
+#################################   Adresse du serveur  ###############################
+### Label pour afficher 'Adresse du serveur'
+Label3 = Label(Mafenetre, text = 'Adresse du serveur ')
+Label3.pack(side = LEFT, padx = 5, pady = 5)
 
-            if message_recu =='' or message_recu.upper() == "FIN":
-                iKillYouBloodyThread=1
-        
-        print ("Adios!")
-        iKillYouBloodyThread=1
-        self.connexion.close()
-    
-#------------------------------------------------------------------------
-class ThreadEmission(threading.Thread):
-    """objet thread gérant l'émission des messages"""
-    def __init__(self, conn):
-        threading.Thread.__init__(self)
-        self.connexion = conn           # réf. du socket de connexion
-        
-    def run(self):
-        global iKillYouBloodyThread     # variable d'arret du thread
-        login = str(sys.argv[1])
-        self.connexion.send(login.encode())
-        time.sleep(3)
+# Création d'un widget Entry (champ de saisie) pour l'adresse du serveur
+addrServ= StringVar()
+Champ3 = Entry(Mafenetre, textvariable= addrServ, bg ='bisque', fg='maroon')
+Champ3.pack(side = LEFT, padx = 5, pady = 5)
 
-        while iKillYouBloodyThread == 0:
-            sys.stdout.flush()
-            message_emis = input("["+login+"]# ")
-            if message_emis.upper()=="FIN":
-                iKillYouBloodyThread=1
-                break
-            if iKillYouBloodyThread == 0:
-                self.connexion.send(message_emis.encode())
-                sys.stdout.flush()
-        
-        
-        iKillYouBloodyThread=1
-        time.sleep(1)
-        print("Amigos!")
-        self.connexion.close()
 
-# ----------------Programme principal - Établissement de la connexion :
-connexion = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:
-    connexion.connect((HOST, PORT))
-except socket.error:
-    print ("La connexion a échoué...")
-    sys.exit()    
-print ("Connexion établie avec le serveur...")
-            
-# Dialogue avec le serveur : on lance deux threads pour gérer
-# indépendamment l'émission et la réception des messages :
+#################################   Port du serveur     ###############################
+### Label pour afficher 'Adresse du serveur'
+Label4 = Label(Mafenetre, text = 'Port du serveur ')
+Label4.pack(side = LEFT, padx = 5, pady = 5)
 
-th_E1 = ThreadEmission(connexion)
-th_R1 = ThreadReception(connexion)
-th_E1.start()
-th_R1.start()
+# Création d'un widget Entry (champ de saisie) pour le port du serveur
+portServ= StringVar()
+Champ4 = Entry(Mafenetre, textvariable= portServ, bg ='bisque', fg='maroon')
+Champ4.pack(side = LEFT, padx = 5, pady = 5)
+
+#################################   Bouton valider  ###############################
+# Création d'un widget Button (bouton Valider)
+Bouton = Button(Mafenetre, text ='Valider', command = Connet2serv)
+Bouton.pack(side = RIGHT, padx = 5, pady = 5)
+
+Mafenetre.mainloop()
